2827779345e69b2d4b19fb0759c958457605e244
agree0002 <donghui1365@outlook.kr>
2025-08-09 16:47:35 +0900

    Chore: 초기 설정

---
+++ src/main/java/ibas/inchelin/InchelinApplication.java
@@ -0,0 +1,13 @@
+package ibas.inchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InchelinApplication.class, args);
+	}
+
+}
