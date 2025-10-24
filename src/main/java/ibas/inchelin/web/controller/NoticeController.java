e3b587273239aab8c69a7beaccc30bfa312b342e
agree0002 <donghui1365@outlook.kr>
2025-10-24 22:53:02 +0900

    Fix: 오타 수정

--- src/main/java/ibas/inchelin/web/controller/NoticeController.java
+++ src/main/java/ibas/inchelin/web/controller/NoticeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController //반환값 JSON
-@RequestMapping("/notices")
+@RequestMapping("/api/notices")
 @RequiredArgsConstructor
 public class NoticeController {
 
